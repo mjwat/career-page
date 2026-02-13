f88d44a5943d5cadd0cff5a44fc1cd5f1b021904
vsakhno <vsakhno@oggettoweb.com>
2026-02-13 12:49:52 +0000

    Add QA career page structure

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 async function loadData() {
   const response = await fetch("data/en.json");
   const data = await response.json();
-  console.log(data);
+
+  const profileEl = document.getElementById("profile");
+  profileEl.innerHTML = `
+    <h1>${data.profile.name}</h1>
+    <p>${data.profile.role}</p>
+  `;
 }
 
 loadData();
